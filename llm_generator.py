cb30443e3fd48702bf80e10622e796707ef52d5b
jwylie <mite51@hotmail.xom>
2024-10-24 23:13:59 -0700

    switched llama code out for a submodule to a forked llama_cpp_python so this will work on both windows and linux

--- llm_generator.py
+++ llm_generator.py
@@ -1,3 +1,8 @@
+import sys
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(dir_path, "llama_cpp_python"))
+
 import llama_cpp
 from llama_cpp._internals import _LlamaTokenDataArray
 
